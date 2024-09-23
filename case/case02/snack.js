1881607cd1493f1546a17ecd2ec48601ba656f39
maile <911464719@qq.com>
2024-09-23 17:30:35 +0800

    34

--- case/case02/snack.js
+++ case/case02/snack.js
@@ -1,7 +1,9 @@
-let time = 25; // 初始速度
+let time = 50; // 初始速度
 let map = document.getElementById('map');
 let timer;
-let socre = 0;
+let score = 0;
+let Score = document.querySelector('.score');
+let foods = [];
 //蛇的构造函数
 function Snake() {
 
@@ -58,8 +60,11 @@ function Snake() {
                 this.body[0].y += 1;
                 break;
         }
+        const mapWidth = map.clientWidth / this.width; // 计算地图宽度的格子数
+        const mapHeight = map.clientHeight / this.height; // 计算地图高度的格子数
+
         //头部碰撞检查
-        if (this.body[0].x < 0 || this.body[0].x > 150 || this.body[0].y < 0 || this.body[0].y > 60)
+        if (this.body[0].x < 0 || this.body[0].x >= mapWidth || this.body[0].y < 0 || this.body[0].y >= mapHeight)
         {
             clearInterval(timer);
             alert("Over Map, Over Game!");
@@ -81,19 +86,22 @@ function Snake() {
             return false;
         }
 
-        // 判断是否吃到食物，坐标重叠，增加蛇的长度，食物刷新
-        if (this.body[0].x == food.x && this.body[0].y == food.y) {
-            this.body.push({ x: null, y: null, flag: null });
-            var len = this.body.length;
-            time = time - (len - 3) * 5; 
-            if (time < 40) {
-                time = 40;
+        // 判断是否吃到食物
+        for (var i = 0; i < foods.length; i++) {
+            if (this.body[0].x == foods[i].x && this.body[0].y == foods[i].y) {
+                time--;
+                score++;
+                generateFoods(score);
+                Score.textContent = "Score:" + score;
+                this.body.push({ x: null, y: null, flag: null }); // 增加蛇的长度
+                var len = this.body.length;
+                time = Math.max(40, time - (len - 3) * 5); // 限制最小速度
+                refresh();
+                map.removeChild(foods[i].flag); // 移除被吃掉的食物
+                foods.splice(i, 1); // 从数组中删除食物
+                break; // 只处理一个食物
             }
-            refresh();
-            map.removeChild(food.flag);
-            food.display();
         }
-
         //撞到自己结束游戏判定
         for (var i = 4; i < this.body.length; i++) {
             if (this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y) {
@@ -146,11 +154,17 @@ function Food() {
         
     };
 }
-
+function generateFoods(num) {
+    for (let i = 0; i < num; i++) {
+        let food = new Food();
+        foods.push(food);
+        food.display();
+    }
+}
 var snake = new Snake();
-var food = new Food();
 snake.display();
-food.display();
+generateFoods(1); // 生成1个初始食物
+
 
 //键盘输入控制
 document.body.onkeydown = function (e) {
