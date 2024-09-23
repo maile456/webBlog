f847217c1f3389d7417065e02ad5a487e3edfe47
maile <911464719@qq.com>
2024-09-23 11:23:15 +0800

    32

---
+++ case/case02/snack.js
@@ -0,0 +1,196 @@
+var time = 500; // 初始速度
+var map = document.getElementById('map');
+var timer;
+
+function Snake() {
+    this.width = 10;
+    this.height = 10;
+    this.direction = 'right'; 
+
+    this.body = [
+        { x: 2, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 0 }
+    ];
+
+    this.display = function () {
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.body[i].x != null) {
+                var s = document.createElement('div');
+                this.body[i].flag = s;
+                s.style.width = this.width + 'px';
+                s.style.height = this.height + 'px';
+                s.style.backgroundColor = 'yellow';
+                s.style.position = 'absolute';
+                s.style.left = this.body[i].x * this.width + 'px';
+                s.style.top = this.body[i].y * this.height + 'px';
+                map.appendChild(s);
+            }
+        }
+        this.body[0].flag.style.backgroundColor = 'orange'; 
+    };
+
+    this.run = function () {
+        for (var i = this.body.length - 1; i > 0; i--) {
+            this.body[i].x = this.body[i - 1].x;
+            this.body[i].y = this.body[i - 1].y;
+        }
+        switch (this.direction) {
+            case "left":
+                this.body[0].x -= 1;
+                break;
+            case "right":
+                this.body[0].x += 1;
+                break;
+            case "up":
+                this.body[0].y -= 1; // 添加缺失的逻辑
+                break;
+            case "down":
+                this.body[0].y += 1;
+                break;
+        }
+        if (this.body[0].x < 0 || this.body[0].x > 150 || this.body[0].y < 0 || this.body[0].y > 60) {
+            clearInterval(timer);
+            alert("Over Map, Over Game!");
+            document.getElementById('beginBox').style.display = 'block';
+
+            for (var i = 0; i < this.body.length; i++) {
+                if (this.body[i].flag != null) {
+                    map.removeChild(this.body[i].flag);
+                }
+            }
+            this.body = [
+                { x: 2, y: 0 },
+                { x: 1, y: 0 },
+                { x: 0, y: 0 }
+            ];
+            this.direction = 'right';
+            this.display();
+            return false;
+        }
+        // 判断是否吃到食物
+        if (this.body[0].x == food.x && this.body[0].y == food.y) {
+            this.body.push({ x: null, y: null, flag: null });
+            var len = this.body.length;
+            time = time - (len - 3) * 5; // 用 time 代替 SnackTime
+            if (time < 40) {
+                time = 40;
+            }
+            refresh();
+            map.removeChild(food.flag);
+            food.display();
+        }
+
+        for (var i = 4; i < this.body.length; i++) {
+            if (this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y) {
+                alert('You hit yourself!');
+                document.getElementById('beginBox').style.display = 'block'; // 修正 dispaly 错误
+                for (var j = 0; j < this.body.length; j++) {
+                    if (this.body[j].flag != null) {
+                        map.removeChild(this.body[j].flag);
+                    }
+                }
+                this.body = [
+                    { x: 2, y: 0 },
+                    { x: 1, y: 0 },
+                    { x: 0, y: 0 }
+                ];
+                this.direction = 'right';
+                this.display();
+                return false;
+            }
+        }
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.body[i].flag != null) {
+                map.removeChild(this.body[i].flag);
+            }
+        }
+        this.display();
+    };
+}
+
+function Food() {
+    this.width = 10;
+    this.height = 10;
+    this.display = function () {
+        var f = document.createElement('div');
+        this.flag = f;
+        f.style.width = this.width + 'px';
+        f.style.height = this.height + 'px';
+        f.style.background = 'red';
+        f.style.position = 'absolute';
+        this.x = Math.floor(Math.random() * 80);
+        this.y = Math.floor(Math.random() * 40);
+        f.style.left = this.x * this.width + 'px';
+        f.style.top = this.y * this.height + 'px';
+        map.appendChild(f);
+    };
+}
+
+var snake = new Snake();
+var food = new Food();
+snake.display();
+food.display();
+
+document.body.onkeydown = function (e) {
+    var ev = e || window.event;
+    switch (ev.keyCode) {
+        case 38:
+            if (snake.direction != 'down') {
+                snake.direction = "up";
+            }
+            break;
+        case 40:
+            if (snake.direction != "up") {
+                snake.direction = "down";
+            }
+            break;
+        case 37:
+            if (snake.direction != "right") {
+                snake.direction = "left";
+            }
+            break;
+        case 39:
+            if (snake.direction != "left") {
+                snake.direction = "right";
+            }
+            break;
+        // WASD 键
+        case 87:
+            if (snake.direction != "down") {
+                snake.direction = "up";
+            }
+            break;
+        case 83:
+            if (snake.direction != "up") {
+                snake.direction = "down";
+            }
+            break;
+        case 65:
+            if (snake.direction != "right") {
+                snake.direction = "left";
+            }
+            break;
+        case 68:
+            if (snake.direction != "left") {
+                snake.direction = "right";
+            }
+            break;
+    }
+};
+
+var btn = document.getElementById('begin');
+btn.onclick = function () {
+    var parent = this.parentNode;
+    parent.style.display = 'none';
+    timer = setInterval(function () {
+        snake.run();
+    }, time);
+};
+
+function refresh() {
+    clearInterval(timer);
+    timer = setInterval(function () {
+        snake.run();
+    }, time);
+}
